dd2281e57d4a0b7f85da08e8643ed83ac15a6614
Andrew Krause <aek.krause@gmail.com>
2021-11-27 21:52:45 -0600

    Finished incorporating API. Still need to add login features.

--- ParkFinder/Parks/state_park.py
+++ ParkFinder/Parks/state_park.py
@@ -14,19 +14,61 @@
 #
 class state_park():
     
-    def __init__(self, full_name, description, latitude, longitude, states, directionsUrl, image):
+    # Create an initialization function (similar to a constructor) in order to
+    # create a new state_park() object when needed. This will be added to a list.
+    def __init__(self, full_name, description, latitude, longitude, states, directions_url, image):
         
         self.full_name = full_name
         self.description = description
         self.latitude = latitude
         self.longitude = longitude
         self.states = states
-        self.directionsUrl = directionsUrl
+        self.directions_url = directions_url
         self.image = image
-        
-    # def 
     
-    # state name: kkkkkk state parks: {kkkkkkk}
+    # Create GETTERS for the state_park() class.
+    def get_name(self):
+        return self.full_name;
+    
+    def get_description(self):
+        return self.description;
+    
+    def get_latitude(self):
+        return self.latitude;
+    
+    def get_longitude(self):
+        return self.longitude;
+    
+    def get_states(self):
+        return self.states;
+    
+    def get_directions(self):
+        return self.directionsUrl;
+    
+    def get_image(self):
+        return self.image;
+    
+    # Create SETTERS for the state_park() class.
+    def set_name(self, new_name):
+        self.full_name = new_name;
+    
+    def set_description(self, new_description):
+        self.description = new_description;
+    
+    def set_latitude(self, new_latitude):
+        self.latitude = new_latitude;
+    
+    def set_longitude(self, new_longitude):
+        self.longitude = new_longitude;
+    
+    def set_states(self, new_states):
+        self.states = new_states;
+    
+    def set_directions(self, new_directions_url):
+        self.directions_url = new_directions_url;
+    
+    def set_image(self, new_image):
+        self.image = new_image;
         
         
         
--- ParkFinder/Parks/views.py
+++ ParkFinder/Parks/views.py
@@ -43,73 +43,121 @@ def find_parks(request):
 """
 def parks(request):
     
-    # Here we use the National Park Services (NPS) API to obtain all of
+    # -------------------------------------------------------------------------
+    # NOTE: Here we use the National Park Services (NPS) API to obtain all of
     # the parks a selected state. WE MAY WANT TO ADD THIS OUTSIDE OF VIEWS
     # SO WE DO NOT EXCEED OUR LIMIT OF 1,000 REQUESTS PER HOUR TO THE NPS API.
-    
     # -------------------------------------------------------------------------
-
-    # --> Configure API request - GET THIS DONE!!!
 
     # First use the installed django extension for the api, called natlparks.
+    # A call to the API is made here.
     parks = NatlParks("XApMBEDd2SIXa1aX7bakVem1E28W0wWgtHuGn3SF")
     
-    # STEP 1 (API): Create a list variable to store the data 
-    # returned from the api request.
+    # Create a list variable to store the data 
+    # returned from the api request. Obtain the
+    # data from the NPS API.
     nps_data_to_parse = []
-    nps_data_to_parse = parks.parks(limit=1, start=1)
-    nps_data_dict = {}
-    nps_data_dict = nps_data_to_parse['data']
-    nps_first_element = nps_data_dict[0]
+    NUMBER_OF_PLACES_IN_API = 464
+    nps_data_to_parse = parks.parks(limit=NUMBER_OF_PLACES_IN_API, start=1)
     
-    # STEP 2 (API): Obtain specific data attributes from the data dictionary.
-    nps_name_attribute = nps_first_element['fullName']
-    nps_description_attribute = nps_first_element['description']
-    nps_latitude_attribute = nps_first_element['latitude']
-    nps_longitude_attribute = nps_first_element['longitude']
-    nps_states_attribute = nps_first_element['states']
-    nps_directions_attribute = nps_first_element['directionsUrl']
-    
-    nps_images_list = nps_first_element['images']
-    nps_image_element = nps_images_list[0]
-    nps_image_attribute = nps_image_element['url']
-
-    # --> DEBUGGING PRINT STATEMENTS
-    # print(nps_name_attribute)
-    # print(nps_description_attribute)
-    # print(nps_latitude_attribute)
-    # print(nps_longitude_attribute)
-    # print(nps_states_attribute)
-    # print(nps_directions_attribute)
-    # print(nps_image_attribute)
-    
-    # STEP 3 (API): Place the obtained data in a dictionariy that 
-    # will be passed to the frontend of the web application.
-    nps_data_to_display = {}
-    nps_data_to_display["fullName"] = nps_name_attribute
-    nps_data_to_display["description"] = nps_description_attribute
-    nps_data_to_display["latitude"] = nps_latitude_attribute
-    nps_data_to_display["longitude"] = nps_longitude_attribute
-    nps_data_to_display["states"] = nps_states_attribute
-    nps_data_to_display["directionsUrl"] = nps_directions_attribute
-    nps_data_to_display["image"] = nps_image_attribute
+    # Create a second list to pass to the frontend 
+    # when the data from the API is obtained.
+    nps_data_list = []
 
     # -------------------------------------------------------------------------
+    # Populate a list with dictionary objects using a loop.
+    # -------------------------------------------------------------------------
 
-    # STEP 4 (OTHER): Add a key value pair to the nps data dictionary, 
-    # which is the name of the state.  
-    state_name = request.POST['state'].strip()
-    nps_data_to_display["state_name"] = state_name
+    # Create variables that determine the number of 
+    # iterations performed by the while loop.
+    NUMBER_OF_PARKS = NUMBER_OF_PLACES_IN_API # --> THIS SHOULD BE THE MAXIMUM NUMBER OF PLACES IN THE FINAL VERSION OF THE PROJECT!!!
+    i = 0
     
-    # Also add the state abbreviation, called
-    # state-code, for use in the frontend. 
-    state_code = request.POST['state-code'].strip()
-    nps_data_to_display["state_code"] = state_code
-
+    # Create a dictionary element to store the data from the JSON file.
+    # The dictionary will be added to a list of dictionary objects.
+    # This dictionary is only used to access all of the data in the json
+    # file.
+    nps_data_dict = {}
+    nps_data_dict = nps_data_to_parse['data']
+    
+    # The while loop analyzes the data in the JSON file 
+    # returned by the API, dividing it and adding it as
+    # an object of the state_park class to a list.
+    while(i < NUMBER_OF_PARKS):
+        
+        # Obtain the packaged information for a given park/place in the JSON file.
+        nps_first_element = nps_data_dict[i]
+        
+        # Obtain specific data attributes from the data dictionary.
+        nps_id_attribute = nps_first_element['id']
+        nps_name_attribute = nps_first_element['fullName']
+        nps_description_attribute = nps_first_element['description']
+        nps_latitude_attribute = nps_first_element['latitude']
+        nps_longitude_attribute = nps_first_element['longitude']
+        nps_states_attribute = nps_first_element['states']
+        nps_directions_attribute = nps_first_element['directionsUrl']
+        
+        # Obtain image data from the API.
+        nps_images_list = nps_first_element['images']
+        nps_image_element = nps_images_list[0]
+        nps_image_attribute = nps_image_element['url']
+        
+        # Place the obtained data in a dictionary that will
+        # be passed to the frontend of the web application.
+        nps_data_to_display = {}
+        
+        # Add a key value pair to the nps data dictionary, 
+        # which is the name of the state. This action is only 
+        # completed once, which is indicated by the conditional.
+        if(i == 0):
+            state_name = request.POST["state"].strip()
+            nps_data_to_display["state_name_display"] = state_name
+            
+            # Also add the state abbreviation, called
+            # state-code, for use in the frontend. --> MAYBE DELETE LATER!!!
+            state_code = request.POST["state-code"].strip()
+            nps_data_to_display["state_code"] = state_code
+        
+        # Populate the dictionary with values to be displayed 
+        # on the frontend of the website.
+        nps_data_to_display["id"] = nps_id_attribute
+        nps_data_to_display["fullName"] = nps_name_attribute
+        nps_data_to_display["description"] = nps_description_attribute
+        nps_data_to_display["latitude"] = nps_latitude_attribute
+        nps_data_to_display["longitude"] = nps_longitude_attribute
+        nps_data_to_display["states"] = nps_states_attribute
+        nps_data_to_display["directionsUrl"] = nps_directions_attribute
+        nps_data_to_display["image"] = nps_image_attribute
+        
+        # Add the state name, for use in the frontend, to
+        # every dictionary in order to perform checks. --> MAYBE DELETE LATER!!!
+        state_name = request.POST["state"].strip()
+        nps_data_to_display["state_name"] = state_name
+            
+        # Also add the state abbreviation, called
+        # state-code, for use in the frontend. 
+        state_code = request.POST["state-code"].strip()
+        nps_data_to_display["state_code"] = state_code
+        
+        # Push the dictionary to a list that will be passed into the frontend.
+        nps_data_list.append(nps_data_to_display)
+        
+        # Increment the counter variable.
+        i += 1
+        
+    # -------------------------------------------------------------------------
+    # End of loop used to populate list with dictionary objects.
+    # -------------------------------------------------------------------------
+    
+    # Create a variable to pass the nps_data_list to the frontend.
+    park_list = {
+            'parklist': nps_data_list,
+    }
+    
     # If the method is a post requst, render the data for the park on 
     # screen. Otherwise, redirect to the find parks page of the website.
     if request.method == "POST":
-        return render(request, 'Parks/pages/parks.html', nps_data_to_display)
+        return render(request, 'Parks/pages/parks.html', park_list)
     else:
         return redirect("ParkFinder-find_parks")
 
@@ -118,177 +166,3 @@ def parks(request):
 """
 def specific_park(request):
     return render(request, 'Parks/pages/specific_park.html')
-
-
-#######################################################################
-
-
-# """
-#     Author: Riley Radle
-#     Description:
-#     Views.py is responsible for routing to and from the html pages.
-#     It handles requests from the user, querying the database, and more.
-#     This is the main backend file which handles the information about the store.
-# """
-
-# from django.shortcuts import render
-# from .models import Item, Category
-# from .utils import utils
-
-
-# """
-#     NOTE: 
-#     @param 'request' is a string representation 
-#     of the request that is generated from the html pages.
-#     Each view get's their own request object.
-# """
-
-# """
-#     Render the home html page to the browser.
-# """
-# def home(request):
-#     # DO NOT UNCOMMENT, used to quickly/automatically add items to database.
-#     # utils.add_database_categories()
-#     # utils.add_database_items()
-#     return render(request, "Store/pages/home.html")
-
-
-# """
-#     Render the login html page to the browser.
-# """ 
-# def login(request):
-#     return render(request, "Store/pages/login.html")
-
-
-# """
-#     Display all of the items that have the given category.
-#     @param name: The name of the category from which to get and display items. 
-# """ 
-# def view_category(request, name):
-#     # Get the items associated with the category for displaying
-#     data_to_display = {
-#         "category_items" : utils.get_category_items(name)
-#     }
-
-#     return render(request, "Store/pages/view_category.html", data_to_display)
-
-
-# """
-#     Display the items that match the user's search. 
-# """
-# def search_results(request): 
-
-#     # The user searched for an item.
-#     if request.method == "POST":
-#         data_to_display = {}
-
-#         # Strip the whitespace off of the user's search and make it lowercase
-#         # Then get the search results
-#         search_string = request.POST['searched_string'].strip().lower()
-#         search_results = utils.get_search_results(search_string)
-
-#         # Record the data for displaying
-#         data_to_display["search_string"] = search_string
-#         data_to_display["search_results"] = search_results
-
-#         return render(request, "Store/pages/search_results.html", data_to_display)
-
-#     # The user did not search for an item.
-#     return render(request, "Store/pages/search_results.html")
-
-
-# """
-#     Display a single item and all of it's relevant information. 
-#     @param name: The name of the item to display.
-# """
-# def view_item(request, name):
-
-#     # Query the database for the current item.
-#     current_item = Item.objects.get(pk=name)
-#     data_to_display = {
-#         "current_item" : current_item
-#     }
-
-#     # The user tried to add or remove an item from cart
-#     if request.method == "POST":
-
-#         # If the item was in the cart, the user is removing it
-#         if current_item.in_cart:
-#             utils.remove_from_cart(current_item)
-
-#         # Else they must have been adding the item
-#         else:
-#             utils.add_to_cart(current_item)
-         
-#     return render(request, "Store/pages/view_item.html", data_to_display)
-
-
-# """
-#     Display the items in the current user's cart.
-#     @param name: The name of an item to remove from cart (if the user wishes to do so) 
-# """
-# def view_cart(request, name=None):
-
-#     # User wants to remove an item from the cart
-#     if request.method == "POST":
-#         item = Item.objects.get(pk=name)
-#         utils.remove_from_cart(item)
-
-#     # Get the cart items and determine the total
-#     cart = utils.get_cart_items()
-#     total = utils.sum_prices(cart)
-
-#     # Dictionary of data to display in the html
-#     data_to_display = {
-#         "cart" : cart,
-#         "total" : "%.2f"%total
-#     }
-
-#     return render(request, "Store/pages/view_cart.html", data_to_display)
-
-
-# """
-#     Display the items in the user's cart for checking out. 
-# """
-# def checkout(request):
-#     # Get the cart items and determine the total
-#     cart = utils.get_cart_items()
-#     total = utils.sum_prices(cart)
-
-#     # Determine the subtotal
-#     subtotal = total + (total * utils.tax_rate) + utils.shipping
-#     tax = total * utils.tax_rate 
-
-#     # Dictionary of data to display in the html
-#     data_to_display = {
-#         "cart" : cart,
-#         "total" : "%.2f"%total, 
-#         "subtotal" : "%.2f"%subtotal,
-#         "tax" : "%.2f"%tax,
-#         "shipping" : "%.2f"%utils.shipping 
-#     }    
-
-#     return render(request, "Store/pages/checkout.html", data_to_display)
-
-# """
-#     Display the items that match the user's search. 
-# """
-# def search_results(request): 
-
-#     # The user searched for an item.
-#     if request.method == "POST":
-#         data_to_display = {}
-
-#         # Strip the whitespace off of the user's search and make it lowercase
-#         # Then get the search results
-#         search_string = request.POST['searched_string'].strip().lower()
-#         search_results = utils.get_search_results(search_string)
-
-#         # Record the data for displaying
-#         data_to_display["search_string"] = search_string
-#         data_to_display["search_results"] = search_results
-
-#         return render(request, "Store/pages/search_results.html", data_to_display)
-
-#     # The user did not search for an item.
-#     return render(request, "Store/pages/search_results.html")
