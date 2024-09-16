ff315e627950ed3251379f8e387c1e7125cad704
Andrew Krause <krause3004@uwlax.edu>
2024-09-16 04:15:22 -0500

    Updated settings to use newer version of python.

--- ParkFinder/ParkFinder/settings.py
+++ ParkFinder/ParkFinder/settings.py
@@ -26,6 +26,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Collect all static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
