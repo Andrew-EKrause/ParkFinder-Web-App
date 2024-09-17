46e5ad233154a427cf66e90f9b2cf5eb7aa3dc4d
Andrew Krause <krause3004@uwlax.edu>
2024-09-16 23:12:44 -0500

    Updated lock file.

--- ParkFinder/ParkFinder/settings.py
+++ ParkFinder/ParkFinder/settings.py
@@ -21,7 +21,7 @@
 """
 
 from pathlib import Path
-import django_heroku
+#import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
